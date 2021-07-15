4630e719622717a3482f8422446bae1fcb319af3
the-Manx <bryandgordon@hotmail.com>
2021-07-14 20:36:53 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tile Game - Kids Can Code
+Kids Can Code Tile Game
+https://www.youtube.com/watch?v=3UxnelT9aCo
---
+++ settings.py
@@ -0,0 +1,20 @@
+#define colours
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+
+# pyGame Template Settings
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+TITLE = "Tilemap Demo"
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
